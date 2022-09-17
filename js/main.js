c3ee77094019f8eae260304fc5377de7af14ef94
Khrustitski Daniil <hrustitskij@yandex.ru>
2022-09-17 10:23:47 +0300

    Reduce

--- js/main.js
+++ js/main.js
@@ -1,32 +1,52 @@
-const products = [
-    {id: 1, img: 'img/notebook.jpg', title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, img: 'img/gamepad.jpg', title: 'Gamepad', price: 50},
-];
-//Функция для формирования верстки каждого товара
-//Добавить в выводе изображение
-
-const renderProduct = (item) => {
-    if(!item.img) {
-        item.img = 'img/placeholder.png';
+class ProductList {
+    constructor(container = '.products'){
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();//рекомендация, чтобы метод был вызван в текущем классе
+        this.render();//вывод товаров на страницу
+    }
+    _fetchProducts(){
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 2000},
+            {id: 2, title: 'Mouse', price: 20},
+            {id: 3, title: 'Keyboard', price: 200},
+            {id: 4, title: 'Gamepad', price: 50},
+        ];
+    }
+    
+    render(){
+        const block = document.querySelector(this.container);
+        for(let product of this.goods){
+             const item = new ProductItem(product);
+             block.insertAdjacentHTML("beforeend",item.render());                
+        }
     }
 
-    return `<div class="product-item">
-                <img src="${item.img}" alt="${item.title}">
-                <h3>${item.title}</h3>
-                <p>$${item.price}</p>
-                <button class="buy-btn">Купить</button>
-            </div>`
-};
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item));
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList.join('');
-    /**
-     * Запятые появляются, потому что выводится массив. Метод join объединяет
-     * массив в строку, разделенную аргументом, указанным в скобках
-     */
-};
+    totalGoodsSum(){
+        const startValue = 0;
+        return this.goods.reduce((acc, value) => acc + value.price, startValue);
+    }
+}
 
-renderPage(products);
+class ProductItem {
+    constructor(product, img = 'img/placeholder.png'){
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = img;
+    }
+    render(){
+        return `<div class="product-item">
+                    <img src="${this.img}" alt="${this.title}">
+                    <h3>${this.title}</h3>
+                    <p>$${this.price}</p>
+                    <button class="buy-btn">Купить</button>
+                </div>`;
+    }
+}
+
+
+
+let list = new ProductList();
+
+console.log(list.totalGoodsSum());
